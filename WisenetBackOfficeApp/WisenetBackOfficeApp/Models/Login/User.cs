d10e78c4b07bacb6a705c2fcdeff57fbddc62654
Ricardo Dzul <near31_3112@hotmail.com>
2017-03-31 12:22:51 -0600

    Agreando clase de usuario para login

---
+++ WisenetBackOfficeApp/WisenetBackOfficeApp/Models/Login/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WisenetBackOfficeApp.Models.Login
+{
+    class User
+    {
+        public int IdUsurio { get; set; }
+        public int? UserName { get; set; }
+        public string Password { get; set; }
+        public string Nombres { get; set; }
+    }
+}
