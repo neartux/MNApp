2907c9199a5624586dcb5c30849feb0dd4f5d3bc
Ricardo Dzul <near31_3112@hotmail.com>
2017-04-04 17:34:02 -0600

    Eliminando paquetes inecessarios

--- WisenetBackOfficeApp/WisenetBackOfficeApp/Models/Login/User.cs
+++ WisenetBackOfficeApp/WisenetBackOfficeApp/Models/Login/User.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WisenetBackOfficeApp.Models.Login
+﻿namespace WisenetBackOfficeApp.Models.Login
 {
     class User
     {
