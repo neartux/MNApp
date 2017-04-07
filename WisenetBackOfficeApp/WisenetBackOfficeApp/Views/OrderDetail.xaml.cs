0f9ac58072b5870ef69878a29f44024c855e6b63
Ricardo Dzul <near31_3112@hotmail.com>
2017-04-07 11:52:29 -0500

    Archivos para detalle de orden

---
+++ WisenetBackOfficeApp/WisenetBackOfficeApp/Views/OrderDetail.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WisenetBackOfficeApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OrderDetail : TabbedPage
+    {
+        public OrderDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
